1f20ed6acec575d45cf521e505a5f57786724bc4
Batmend12 <batmendbatbaatar12@gmail.com>
2025-02-07 11:14:54 +0800

    comment

--- pages/lab1.js
+++ pages/lab1.js
@@ -11,8 +11,8 @@ export default function home() {
 
   return (
       <div onClick={() => setClick((test) => !test)}className="flex items-center flex-col h-screen justify-center">
-        <p className="border">Daraad uz</p>
-        <p>{click == true ? "ok" : ""}</p>
+        <p className="border rounded-xl p-7">Dont Click This At 3 Am</p>
+        <p>{click == true ? "You just became an impostor in amongus" : ""}</p>
       </div>
   );
 }
---
+++ pages/lab2.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const UseStateTask = () => {
+  const [color, setColor] = useState("");
+
+  return (
+    <div className={`flex space-x-4 w-full h-screen bg-${color}-800`}>
+      <p>Өнгө сонгох:</p>
+      <button
+        className="border bg-red-800 py-4 px-6"
+        onClick={() => setColor("red")}
+      >
+        Улаан
+      </button>
+      <button
+        className="border bg-blue-800 py-4 px-6"
+        onClick={() => setColor("blue")}
+      >
+        Цэнхэр
+      </button>
+      <button
+        className="border bg-green-800 py-4 px-6"
+        onClick={() => setColor("green")}
+      >
+        Ногоон
+      </button>
+    </div>
+  );
+};
+
+export default UseStateTask;
