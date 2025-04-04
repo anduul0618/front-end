d250f5966b1eb92e76d365625c6d27c5ea37af72
Batmend12 <batmendbatbaatar12@gmail.com>
2025-04-04 09:57:50 +0800

    ohio

--- pages/lab7.js
+++ pages/lab7.js
@@ -330,14 +330,14 @@ export default function Lab4() {
           style={{ backgroundImage: "url('/nestlogo.jpg')", backgroundAttachment: "fixed" }}
         >
       <button
-        className="absolute top-6 right-6 font-bold text-xl bg-gray-800 text-white px-6 py-2 rounded-lg shadow-md 
+        className="absolute top-6 right-6 font-bold text-xl bg-gray-800 text-white px-6 py-2 rounded-lg shadow-md z-50
                    hover:bg-gray-600 transition transform hover:scale-105"
         onClick={() => router.push("/")}
       >
         Back
       </button>
       <div className="pt-10 min-h-screen px-4 bg-transparent backdrop-blur-sm">
-        <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">11c Students</h1>
+        <h1 className="text-3xl font-bold text-center mb-6 text-gray-white">11c Students</h1>
         <div className="flex justify-between items-center mb-6 max-w-5xl mx-auto">
           <input
             type="text"
